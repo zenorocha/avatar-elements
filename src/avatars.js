7521f6ebbf0a1c6b9e0134c0349249b98f4d9977
Zeno Rocha <hi@zenorocha.com>
2015-03-26 12:19:20 -0300

    Moves reusable logics to interface class

--- src/avatars.js
+++ src/avatars.js
@@ -1,7 +1,21 @@
 'use strict';
 
 class Avatar extends HTMLImageElement {
-    loadJSON(path, callback) {
+    username() {
+        var usernameAttr = this.getAttribute('username');
+
+        if (!usernameAttr) {
+            throw new Error('Username attribute is required.');
+        }
+
+        return usernameAttr;
+    }
+
+    size() {
+        return this.getAttribute('size') || 48;
+    }
+
+    load(path, callback) {
     	var httpRequest = new XMLHttpRequest();
     	httpRequest.onreadystatechange = function() {
     		if (httpRequest.readyState === 4) {
@@ -17,21 +31,30 @@ class Avatar extends HTMLImageElement {
 }
 
 class GitHubAvatar extends Avatar {
+    apiURL() {
+        return 'https://api.github.com/users/' + this.username();
+    }
+
+    imageURL(data) {
+        return data.avatar_url + '&s=' + this.size();
+    }
+
     createdCallback() {
-        var self = this,
-            size = this.getAttribute('size') || 48,
-            username = this.getAttribute('username');
+        var self = this;
 
-        if (username) {
-            var url = 'https://api.github.com/users/' + username;
-        } else {
-            throw new Error('Username attribute is required.');
-        }
-
-        self.loadJSON(url, function(data) {
-            var image = data.avatar_url + '&s=' + size;
-            self.setAttribute('src', image);
+        self.load(self.apiURL(), function(data) {
+            self.setAttribute('src', self.imageURL(data));
 		});
+    }
+}
+
+class FacebookAvatar extends Avatar {
+    imageURL() {
+        return 'https://graph.facebook.com/' + this.username() + '/picture?' + 'width=' + this.size() + '&height=' + this.size();
+    }
+
+    createdCallback() {
+        this.setAttribute('src', this.imageURL());
     }
 }
 
@@ -39,23 +62,6 @@ document.registerElement('avatar-github', {
     prototype: GitHubAvatar.prototype,
     extends: 'img'
 });
-
-
-
-class FacebookAvatar extends Avatar {
-  createdCallback(){
-    var self = this,
-        size = this.getAttribute('size') || 48,
-        username = this.getAttribute('username');
-
-        if(username){
-            var url = 'https://graph.facebook.com/' + username + '/picture?' + 'width=' + size + '&height=' + size;
-            self.setAttribute('src',url);
-        } else {
-          throw new Error('Username attribute is required.');
-        }
-  }
-}
 
 document.registerElement('avatar-facebook', {
     prototype: FacebookAvatar.prototype,
