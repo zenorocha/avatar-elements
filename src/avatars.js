b6e2de06e6c6b163abac3c1504a3c88ebcc17300
Joridos <jonatam.ribeiro@hotmail.com>
2015-03-26 16:02:53 -0300

    Adds Skype avatar Closes #5

--- src/avatars.js
+++ src/avatars.js
@@ -58,6 +58,16 @@ class FacebookAvatar extends Avatar {
     }
 }
 
+class SkypeAvatar extends Avatar {
+    imageURL() {
+        return `http://api.skype.com/users/${this.username()}/profile/avatar`;
+    }
+
+    createdCallback() {
+        this.setAttribute('src', this.imageURL());
+    }
+}
+
 document.registerElement('avatar-github', {
     prototype: GitHubAvatar.prototype,
     extends: 'img'
@@ -65,5 +75,10 @@ document.registerElement('avatar-github', {
 
 document.registerElement('avatar-facebook', {
     prototype: FacebookAvatar.prototype,
+    extends: 'img'
+});
+
+document.registerElement('avatar-skype', {
+    prototype: SkypeAvatar.prototype,
     extends: 'img'
 });
